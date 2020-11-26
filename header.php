800c8b74acfd382a128e709cafc64d6a272256dc
Mohamed Baroky <m.baroky@ype-dc01.dom>
2020-11-26 16:24:25 +0200

    second commit

---
+++ header.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="ar">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>الملكة العقارية</title>
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:description" content="">
+    <meta property="og:type" content="website">
+    <meta name="description" content="">
+    <meta name="twitter:image" content="assets/img/logoo-maleka.png">
+    <meta property="og:image" content="assets/img/logoo-maleka.png">
+    <meta property="og:title" content="الملكة العقارية">
+    <meta name="twitter:title" content="الملكة العقارية">
+    <link rel="icon" type="image/png" sizes="252x170" href="assets/img/logoo-maleka.png">
+    <link rel="icon" type="image/png" sizes="252x170" href="assets/img/logoo-maleka.png">
+    <link rel="icon" type="image/png" sizes="252x170" href="assets/img/logoo-maleka.png">
+    <link rel="icon" type="image/png" sizes="252x170" href="assets/img/logoo-maleka.png">
+    <link rel="icon" type="image/png" sizes="252x170" href="assets/img/logoo-maleka.png">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cairo">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Changa">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Josefin+Sans:300,400,700">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,900">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/fonts/line-awesome.min.css">
+    <link rel="stylesheet" href="assets/fonts/simple-line-icons.min.css">
+    <link rel="stylesheet" href="assets/css/contact-html.css">
+    <link rel="stylesheet" href="assets/css/flex-slider.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flexslider/2.7.2/flexslider.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/css/lightbox.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.3.1/css/swiper.min.css">
+    <link rel="stylesheet" href="https://unpkg.com/aos@2.3.1/dist/aos.css">
+    <link rel="stylesheet" href="assets/css/icon-anim.css">
+    <link rel="stylesheet" href="assets/css/language-mobile.css">
+    <link rel="stylesheet" href="assets/css/Lightbox-Gallery.css">
+    <link rel="stylesheet" href="assets/css/Social-Icons.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body style="background-size: 0;background-color: #fff;">
+<?php 
+$activePage = basename($_SERVER['PHP_SELF'], ".php");
+function isMobile() {
+    return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
+}
+?>
+
+
+    <header id="main-header" class="bottom" style="border-top: 6px #cc3e3d solid;">
+        <div class="container-fluid" style="background-color: #fff;">
+            <div class="row align-items-stretch justify-content-md-start rtl">
+                <div class="col-3 col-md-5 d-flex flex-column justify-content-between align-items-stretch align-self-center align-self-md-stretch">
+                    <div class="row">
+                        <div class="col p-0">
+                            <nav class="navbar navbar-light navbar-expand-md ltr" id="mainNav" style="background-color: transparent;font-family: Changa, sans-serif;">
+                                <div class="container-fluid"><button data-toggle="collapse" class="navbar-toggler b-0" data-target="#navbarResponsive"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+                                    <div class="collapse navbar-collapse"
+                                        id="navbarResponsive" style="margin-top: 0;">
+                                        <ul class="nav navbar-nav text-right p-0" id="parentNav">
+                                            <li class="nav-item text-center <?= ($activePage == 'index') ? 'active':''; ?>" role="presentation"><span class="d-none d-md-inline style"></span><a class="nav-link float-none main-button" href="index.php"><strong>الرئيسية</strong></a></li>
+                                            <li class="nav-item text-center pl-0 <?= ($activePage == 'contact') ? 'active':''; ?>"
+                                                role="presentation"><span class="d-none d-md-inline style"></span><a class="nav-link main-button" href="contact.php"><strong>اتصل بنا</strong></a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-md-2 offset-md-0 text-center align-self-center p-0" id="logo-wrapper"><a class="d-inline-block" href="index.php"><img class="img-fluid" id="logo" src="assets/img/logoo-maleka-432w.png" width="190px" style="margin-top: 3px;margin-bottom: 5px;"></a></div>
+                <div class="col-3 col-md-1 d-flex d-md-none justify-content-center align-items-center no-padding">
+                    <div class="btn-group d-none d-md-flex lang" role="group"><a class="btn btn-light disabled btn-sm border rounded-0" role="button" href="../<?php echo basename($_SERVER['PHP_SELF']);?>">AR</a><a class="btn btn-light btn-sm border rounded-0" role="button" href="en/<?php echo basename($_SERVER['PHP_SELF']);?>">EN</a></div>
+                    <a
+                        class="btn" role="button" href="tel:01202855556" style="box-shadow: none;"><i class="fa fa-phone" style="font-size: 2em;"></i></a>
+                </div>
+                <div class="col-3 col-md-5 d-flex flex-column justify-content-between align-items-stretch align-self-center align-self-md-stretch">
+                    <div class="row d-none d-md-flex">
+                        <div class="col text-left"><a class="btn" role="button" href="tel:01202855556" style="box-shadow: none;">01202855556&nbsp;<i class="fa fa-phone"></i></a></div>
+                        <div class="col-md-5 align-self-center p-0">
+                            <div class="btn-group d-inline-block social-icons" role="group" style="width: auto;margin-top: 0;"><a class="btn border rounded-circle d-flex float-left align-items-center justify-content-md-center b-0 s-0" role="button" style="margin-right: 10px;" href="https://www.facebook.com/%D8%B4%D8%B1%D9%83%D8%A9-%D8%A7%D9%84%D9%85%D9%84%D9%83%D8%A9-%D8%A7%D9%84%D8%B9%D9%82%D8%A7%D8%B1%D9%8A%D8%A9-108292030626522/"><i class="fa fa-facebook-f" style="background-color: #cc3e3d;"></i></a>
+                                <a
+                                    class="btn border rounded-circle d-flex float-left align-items-center justify-content-md-center b-0 s-0" role="button" href="mailto:elmalekaalaqarea@gmail.com"><i class="fa fa-envelope-o" style="background-color: #cc3e3d;"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!--end of header.php-->
---
+++ index.php
@@ -0,0 +1,92 @@
+<?php include 'header.php'; ?>
+
+    <section class="page-section clearfix no-padding" style="margin: 0;">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col text-center no-padding">
+                    <div class="flexslider" data-animation="fade" data-control-nav="false" data-custom-direction-nav=".custom-navigation a" style="height: auto;" data-pause-on-action="false">
+                        <ul class="list-unstyled slides" style="height: auto;display: block;">
+                            <li class="d-inline-flex justify-content-center align-items-center"><img class="img-fluid" src="assets/img/01.jpg">
+                                <div class="color-overlay"></div>
+                                <div class="slide-inner">
+                                    <div class="text-center text-wrapper">
+                                        <h2 class="slide-title"><strong>باترى هوم للاطارات والبطاريات</strong></h2>
+                                        <p class="text-center slide-text">انطلق بلا قلق<br>نهدف الي خلق علاقات راسخة مع عملاؤنا <br></p><a class="btn btn-primary btn-lg border rounded-0 main-button slide-button no-border" role="button" href="#">تخفيضات 10%</a></div>
+                                </div>
+                            </li>
+                            <li class="d-inline-flex justify-content-center align-items-center"><img class="img-fluid" src="assets/img/02.jpg">
+                                <div class="color-overlay"></div>
+                                <div class="slide-inner">
+                                    <div class="text-center text-wrapper">
+                                        <h2 class="slide-title"><strong>باترى هوم للاطارات والبطاريات</strong></h2>
+                                        <p class="text-center slide-text">انطلق بلا قلق<br>باتري هوم تضمن منتجاتها ضد عيوب الصناعة <br></p><a class="btn btn-primary btn-lg border rounded-0 main-button slide-button no-border" role="button" href="#">تخفيضات 10%</a></div>
+                                </div>
+                            </li>
+                            <li class="d-inline-flex justify-content-center align-items-center"><img class="img-fluid" src="assets/img/03.jpg">
+                                <div class="color-overlay"></div>
+                                <div class="slide-inner">
+                                    <div class="text-center text-wrapper">
+                                        <h2 class="slide-title"><strong>باترى هوم للاطارات والبطاريات</strong></h2>
+                                        <p class="text-center slide-text">انطلق بلا قلق<br>أطار عربيتك = أمنك وأمن عائلتك<br></p><a class="btn btn-primary btn-lg border rounded-0 main-button slide-button no-border" role="button" href="#">تخفيضات 10%</a></div>
+                                </div>
+                            </li>
+                            <li class="d-inline-flex justify-content-center align-items-center"><img class="img-fluid" src="assets/img/04.jpg">
+                                <div class="color-overlay"></div>
+                                <div class="slide-inner">
+                                    <div class="text-center text-wrapper">
+                                        <h2 class="slide-title"><strong>باترى هوم للاطارات والبطاريات</strong></h2>
+                                        <p class="text-center slide-text">انطلق بلا قلق<br>باتري هوم تضمن منتجاتها ضد عيوب الصناعة <br></p><a class="btn btn-primary btn-lg border rounded-0 main-button slide-button no-border" role="button" href="#">تخفيضات 10%</a></div>
+                                </div>
+                            </li>
+                            <li class="d-inline-flex justify-content-center align-items-center"><img class="img-fluid" src="assets/img/05.jpg">
+                                <div class="color-overlay"></div>
+                                <div class="slide-inner">
+                                    <div class="text-center text-wrapper">
+                                        <h2 class="slide-title"><strong>باترى هوم للاطارات والبطاريات</strong></h2>
+                                        <p class="text-center slide-text">انطلق بلا قلق<br>أكثر من ثلاثون سنة خبرة <br><br></p><a class="btn btn-primary btn-lg border rounded-0 main-button slide-button no-border" role="button" href="#">تخفيضات 10%</a></div>
+                                </div>
+                            </li>
+                        </ul>
+                        <div class="custom-navigation"><a class="btn btn-dark btn-lg border rounded-0 flex-prev main-button" role="button" style="color: rgb(255,255,255);margin-right: 50px;float: left;" href="#"><i class="fa fa-angle-left"></i><span></span></a><a class="btn btn-dark btn-lg border rounded-0 flex-next main-button"
+                                role="button" style="float: right;" href="#"><i class="fa fa-angle-right"></i><span></span></a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="text-center bottom">
+        <div class="container">
+            <div class="row">
+                <div class="col text-center">
+                    <h3><strong>نبذة</strong><br></h3>
+                    <p class="lead" style="width: 740px;margin-top: 30px;">جروس كيم للكيماويات المتطورة نعمل على تدعيم السوق الزراعي بكل ما هو جديد ومبتكر على أسس علمية حديثة من الكيماويات الزراعية (اسمدة مركبة - مخلبيات - اسمدة سائلة - منظمات نمو) على أيدي خبراء ومتخصصين مستخدمين أحدث الأجهزة والماكينات
+                        تحت مراقبة معامل الجودة الخاصة بالمصنع للوصول لاعلى جودة وكفاءة.<br></p><a class="btn btn-outline-primary btn-lg d-flex float-none justify-content-between" role="button" href="assets/pdf/cata-00.pdf" style="width: 240px;" target="_blank"><span style="flex: 1;">اعرف المزيد</span><span><i class="fa fa-download"></i></span></a></div>
+            </div>
+        </div>
+    </section>
+    <section style="background: linear-gradient(180deg,rgba(44,68,55,1) 2%,rgba(4,26,9,1) 100%) !important;padding-top: 70px;padding-right: 70px;">
+        <div class="container">
+            <div class="row rtl">
+                <div class="col-md-6">
+                    <h1 style="color: #fff;"><strong>رؤية الشركة</strong><br></h1>
+                    <ul class="text-right">
+                        <li>اسس وحلول علمية .<br></li>
+                        <li>اعلى انتاج واقل تكاليف.<br></li>
+                        <li>بيئة ومحاصيل نظيفة.</li>
+                        <li>منافسة الاسواق العالمية.<br></li>
+                    </ul>
+                </div>
+                <div class="col-md-6">
+                    <h1 style="color: #fff;"><strong>اهداف الشركة</strong><br></h1>
+                    <ul class="text-right">
+                        <li>تقديم احدث ماتوصل اليه العلم في مجال تغذية ووقاية النبات.</li>
+                        <li>الاستفادة من الخبرات الاروبية في استخدام الطرق الحديثة في عملية الانتاج.</li>
+                        <li>تقديم حلول علمية وجذرية للتحديات التي تواجه السوق المصري والعربي.</li>
+                        <li>تحقيق اعلى عائد من المحصول باقل تكلفة ممكنة.</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--end of content-->
+<?php include 'footer.php'; ?>
